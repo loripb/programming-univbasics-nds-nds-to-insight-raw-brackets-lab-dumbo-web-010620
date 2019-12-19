1f9af007044f85fac2fbb362e912539eb26b9b43
Lori Boyd <lori.p.boyd@gmail.com>
2019-12-19 08:28:35 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -20,6 +20,7 @@ def directors_totals(nds)
     director_name = nds[director_index][:name] # stores directors name in a variable
 
     puts director_name
+    puts nds.length
 #    while movie_index < nds[director_index][:movies].length
 #      movie_gross = nds[director_index][:movies][movie_index][:worldwide_gross] # stores worldwide_gross in a variable
 #      movie << movie_gross
