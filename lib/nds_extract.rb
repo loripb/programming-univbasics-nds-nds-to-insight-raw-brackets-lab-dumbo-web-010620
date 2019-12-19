332d1788ce322f7267cc2eebe88be27f397dfe8a
Lori Boyd <lori.p.boyd@gmail.com>
2019-12-19 07:55:24 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -17,6 +17,7 @@ def directors_totals(nds)
   puts nds[0][:movies][0][:worldwide_gross]
 
   while movie_index < nds[0][:movies]
+    puts " ayeeeee $#{nds[0][:movies][movie_index][:worldwide_gross]}"
 
 
   #
