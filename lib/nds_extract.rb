7f7272de33a108f24460c01f73cf4a4b6a08ff63
Lori Boyd <lori.p.boyd@gmail.com>
2019-12-19 08:55:49 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,7 +3,6 @@ require 'directors_database'
 require 'pp'
 
 def directors_totals(nds)
-  pp nds
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
   #
